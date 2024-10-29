074bbbecb63c6a2e450575766c9f41d87d013e42
yukiko0525 <pianox2xwxwx5@gmail.com>
2024-10-29 20:06:27 +0900

    kadai_007をアップロード

---
+++ kadai_007/calc.js
@@ -0,0 +1,17 @@
+nunm = 23
+
+if ((nunm % 3) === 0 && (nunm % 5) === 0) {
+  console.log('3と5の倍数です');
+}
+
+if ((nunm % 3) === 0) {
+  console.log('3の倍数です');
+}
+
+if ((nunm % 5) === 0) {
+  console.log('5の倍数です');
+}
+
+else {
+  console.log(nunm);
+}
